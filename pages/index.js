084899035aa68cdcc87360330de8548db1e4de35
ismailllozcelik <ismailll.ozcelik@gmail.com>
2023-07-02 15:14:23 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <div className="bg-slate-500 text-white p-4 rounded-lg">ismail</div>
+    </div>
+  );
+}
